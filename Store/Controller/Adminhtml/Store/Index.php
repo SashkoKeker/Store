3e4dbdb7e63dc78515399760bf58d70c9d292e04
SashkoKeker <sashasam1999@hotmail.com>
2022-07-26 17:33:04 +0300

    routes+start ui comp

---
+++ Store/Controller/Adminhtml/Store/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alexandr\Store\Controller\Adminhtml\Store;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+
+class Index extends Action
+{
+
+    const ADMIN_RESOURCE = 'Magento_Backend::system';
+
+    public function execute(): ResultInterface
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Alexandr_Store::menu_1');
+        //    ->addBreadcrumb(__('Store'), __('List'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Store locator'));
+
+        return $resultPage;
+
+    }
+}
